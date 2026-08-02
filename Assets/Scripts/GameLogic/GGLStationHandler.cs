83add46bab9a6224fe7ea0bcb0b4f9d8e73c4ca5
Bintang Khalik <bintangkhaliqq@gmail.com>
2026-08-03 00:13:39 +0700

    improved GGL Station Handler
    Increased delay of check
    added limit of 1 to 2 to improve flow of UI

--- Assets/Scripts/GameLogic/GGLStationHandler.cs
+++ Assets/Scripts/GameLogic/GGLStationHandler.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GGLStationHandler : MonoBehaviour
 {
-    private static readonly WaitForSeconds _waitForSeconds0_1 = new(0.1f);
+    private static readonly WaitForSeconds _waitForSeconds0_2 = new(0.2f);
     public List<Transform> GGLStampsParents;
     public List<GameObject> GGLObjects;
 
@@ -18,7 +18,7 @@ public class GGLStationHandler : MonoBehaviour
 
     private void CheckStampAvailability(int index)
     {
-        if (GGLStampsParents[index].childCount == 0)
+        if (GGLStampsParents[index].childCount <= 1)
         {
             Instantiate(GGLObjects[index], GGLStampsParents[index]);
         }
@@ -28,7 +28,7 @@ public class GGLStationHandler : MonoBehaviour
     {
         while (true) 
         {
-            yield return _waitForSeconds0_1;
+            yield return _waitForSeconds0_2;
             CheckStampAvailability(index);
         }
     }
