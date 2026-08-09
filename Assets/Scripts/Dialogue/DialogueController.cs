3dca450153850049d01c9ff9ebb057e16957b247
Bintang Khalik <bintangkhaliqq@gmail.com>
2026-08-09 12:55:39 +0700

    added dialogue System

---
+++ Assets/Scripts/Dialogue/DialogueController.cs
@@ -0,0 +1,163 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class DialogueController : MonoBehaviour
+{
+    [Header("Text Settings")]
+    public float textSpeed;
+    public GameObject speechBubble;
+    public TextMeshProUGUI textbox;
+    public Dialogues currentDialogue;
+
+    private int dialogueIndex;
+    private ClientController clientController;
+    private SpeechBubbleController speechBubbleController;
+    private float lastInputTime = 0f;
+    private readonly float inputCooldown = 0.2f; // 200ms cooldown
+
+    private void Start()
+    {
+        clientController = GetComponentInChildren<ClientController>(true);
+        speechBubbleController = GetComponentInChildren<SpeechBubbleController>(true);
+
+        //LevelManager.Instance.OnDialogueTriggered += SetDialogue;
+        DialogueEventManager.Instance.StartGameEvent += SetDialogue;
+
+        DialogueEventManager.Instance.OnClientEntered += SetClientImage;
+        DialogueEventManager.Instance.OnDialogueStarted += StartDialogue;
+        DialogueEventManager.Instance.OnDialogueCompleted += EndDialogue;
+        DialogueEventManager.Instance.OnDialogueContinued += ContinueDialogue;
+
+        DialogueEventManager.Instance.OnFoodSubmitted += SetEndingDialogue;
+    }
+
+    private void OnDisable()
+    {
+        //LevelManager.Instance.OnDialogueTriggered -= SetDialogue;
+        DialogueEventManager.Instance.StartGameEvent -= SetDialogue;
+
+        DialogueEventManager.Instance.OnClientEntered -= SetClientImage;
+        DialogueEventManager.Instance.OnDialogueStarted -= StartDialogue;
+        DialogueEventManager.Instance.OnDialogueCompleted -= EndDialogue;
+        DialogueEventManager.Instance.OnDialogueContinued -= ContinueDialogue;
+
+        DialogueEventManager.Instance.OnFoodSubmitted -= SetEndingDialogue;
+    }
+
+    public void SetDialogue()
+    {
+        if (LevelManager.Instance.index < LevelManager.Instance.FoodDataList.Count)
+        {
+            SetDialogue(LevelManager.Instance.FoodDataList[LevelManager.Instance.index].Dialogue);
+        }
+    }
+    private void SetDialogue(Dialogues dialogue)
+    {
+        currentDialogue = dialogue;
+        dialogueIndex = 0;
+    }
+
+    private void SetClientImage()
+    {
+        if (clientController != null)
+        {
+            clientController.SetClientImage(currentDialogue.GetSprite());
+        }
+    }
+
+    public void StartDialogue()
+    {
+        if (currentDialogue != null)
+        {
+            StartCoroutine(ShowDialogue());
+        }
+    }
+
+    public IEnumerator ShowDialogue()
+    {
+        ClearTextbox();
+        yield return speechBubbleController.ShowSpeechBubble();
+        yield return TypeLinesCoroutine(currentDialogue.GetLine(dialogueIndex));
+    }
+
+    public void EndDialogue()
+    {
+        StartCoroutine(HideDialogue());
+
+        if (currentDialogue.isFirstDialogue)
+        {
+            currentDialogue.isFirstDialogue = false;
+            LevelManager.Instance.TriggerSendFoodEvent();
+        } 
+        else
+        {
+            DialogueEventManager.Instance.TriggerClientExit();
+        }
+    }
+
+    public IEnumerator HideDialogue()
+    {
+        ClearTextbox();
+        yield return speechBubbleController.HideSpeechBubble();
+    }
+
+    private void TypeLines(string text)
+    {
+        StartCoroutine(TypeLinesCoroutine(text));
+    }
+    private IEnumerator TypeLinesCoroutine(string text)
+    {
+        foreach (char c in text.ToCharArray())
+        {
+            textbox.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    public void ContinueDialogue()
+    {
+        if (Time.time - lastInputTime < inputCooldown)
+            return; // Ignore rapid input
+
+        lastInputTime = Time.time;
+
+
+        if (textbox.text == currentDialogue.GetLine(dialogueIndex))
+        {
+            NextLine();
+        }
+        else
+        {
+            StopAllCoroutines();
+            textbox.text = currentDialogue.GetLine(dialogueIndex);
+        }
+    }
+
+    private void NextLine()
+    {
+        if (dialogueIndex < currentDialogue.GetLineCount() - 1)
+        {
+            dialogueIndex++;
+            ClearTextbox();
+            TypeLines(currentDialogue.GetLine(dialogueIndex));
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    private void ClearTextbox()
+    {
+        textbox.text = string.Empty;
+    }
+
+    private void SetEndingDialogue()
+    {
+        currentDialogue.isFirstDialogue = false;
+        dialogueIndex = 0;
+
+        DialogueEventManager.Instance.TriggerDialogueStart();
+    }
+}
---
+++ Assets/Scripts/Dialogue/Dialogues.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogues 
+{
+    [Header("Speaker Settings")]
+    public string speakersName;
+    public Sprite speakersSprite;
+    public AudioClip voiceClip;
+
+    [Header("Dialogue Lines")]
+    public List<string> startingLines;
+    public List<string> endingLines;
+    public List<string> randomChatter;
+
+    public bool isFirstDialogue = true;
+
+    public string GetRandomChatter()
+    {
+        if (randomChatter.Count == 0)
+            return string.Empty;
+        int randomIndex = Random.Range(0, randomChatter.Count);
+        return randomChatter[randomIndex];
+    }
+
+    public string GetLine(int index)
+    {
+        if (index < 0)
+            return string.Empty;
+
+        if (isFirstDialogue)
+        {
+            if (index >= startingLines.Count)
+                return string.Empty;
+            return startingLines[index];
+        }
+        else
+        {
+            if (index >= endingLines.Count)
+                return string.Empty;
+            return endingLines[index];
+        }
+    }
+
+    public int GetLineCount()
+    {
+        return isFirstDialogue ? startingLines.Count : endingLines.Count;
+    }
+
+    public Sprite GetSprite()
+    {
+        return speakersSprite;
+    }
+}
