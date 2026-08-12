77c713a10c373a09c83a15767e87f0b81a069e1f
Bintang Khalik <bintangkhaliqq@gmail.com>
2026-08-12 23:40:39 +0700

    update level GUI

--- Assets/Scripts/UIController/MagnifierController.cs
+++ Assets/Scripts/UIController/MagnifierController.cs
@@ -5,11 +5,13 @@ public class MagnifierController : MonoBehaviour
     private static readonly int EnabledHash = Animator.StringToHash("Enabled");
     public Camera zoomCamera;
     public GameObject magnifierCanvas;
+    public GameObject RedStripe;
     public Animator animator;
     private bool isMagnifierActive = false;
     public void ToggleMagnifier()
     {
         isMagnifierActive = !isMagnifierActive;
+        RedStripe.SetActive(isMagnifierActive);
         animator.SetBool(EnabledHash, isMagnifierActive);
         zoomCamera.enabled = isMagnifierActive;
 
