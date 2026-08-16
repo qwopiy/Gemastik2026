c6a7a5b848df8ee6ab4dff7b74e3ab216e279ed1
Bintang Khalik <bintangkhaliqq@gmail.com>
2026-08-16 15:40:13 +0700

    update level complete controller

--- Assets/Scripts/UIController/LevelCompleteController.cs
+++ Assets/Scripts/UIController/LevelCompleteController.cs
@@ -12,7 +12,7 @@ public class LevelCompleteController : MonoBehaviour
     private bool isLevel4;
     private void Start()
     {
-        isLevel4 = LevelManager.Instance.Level == 3;
+        isLevel4 = LevelManager.Instance.Level >= 3;
     }
 
     public void SetStats()
@@ -40,41 +40,10 @@ public class LevelCompleteController : MonoBehaviour
     {
         if (isLevel4) 
         {
-            string endingSceneName = GetEndingName();
-            TransitionManager.Instance.GoToScene(endingSceneName);
+            TransitionManager.Instance.GoToScene("EndingScene");
             return;
         }
 
         TransitionManager.Instance.GoToScene("MainMenu");
-    }
-
-    private string GetEndingName() // TODO: Change to specific ending scenes
-    {
-        Endings ending = EndingManager.Instance.currentEnding;
-
-        switch (ending)
-        {
-            case Endings.AllCorrect:
-                return "EndingAllCorrect";
-            case Endings.AllMistake:
-                return "EndingAllMistake";
-            case Endings.Sugar:
-                return "EndingSugar";
-            case Endings.Salt:
-                return "EndingSalt";
-            case Endings.Fat:
-                return "EndingFat";
-            case Endings.ExpiredOrDefect:
-                return "EndingExpiredOrDefect";
-            case Endings.WrongNutritionClaim:
-                return "EndingWrongNutritionClaim";
-            case Endings.WrongCompositionClaim:
-                return "EndingWrongCompositionClaim";
-            case Endings.PerfectSpeedrunner:
-                return "EndingPerfectSpeedrunner";
-            default:
-                Debug.LogError("Unknown ending: " + ending);
-                return "LevelSelect"; // Fallback to LevelSelect if unknown
-        }
     }
 }
