1202f8d6c81cff9a0b8cc64d34d4918d6477a462
Bintang Khalik <bintangkhaliqq@gmail.com>
2026-08-16 20:47:08 +0700

    added bgm trigger to main menu

--- Assets/Scripts/UIController/MainMenuScene/MainMenu.cs
+++ Assets/Scripts/UIController/MainMenuScene/MainMenu.cs
@@ -16,4 +16,21 @@ public class MainMenu : MonoBehaviour
     {
         TransitionManager.Instance.TransitionToGameObject(currentPanel, obj);
     }
+
+    public void StartBGMLevel(int index)
+    {
+        AudioClip clip = index switch
+        {
+            0 => AudioManager.Instance.CFD,
+            1 => AudioManager.Instance.Kantin,
+            2 => AudioManager.Instance.EventCosplay,
+            3 => AudioManager.Instance.Kantor,
+            _ => null
+        };
+
+        if (clip != null)
+        {
+            AudioManager.Instance.PlayMusic(clip);
+        }
+    }
 }
